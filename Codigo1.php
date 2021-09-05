09c7b7238bf8cec22d5ebb31642143f899642145
Lucas <lucasferr6@gmail.com>
2021-09-05 20:05:59 -0300

    Segundo commit: A mitad de camino

--- Codigo1.php
+++ Codigo1.php
@@ -2,3 +2,21 @@
 <!--Consigna:
 Desarrolle una pagina en la cual se puedan 
 obtener aleatoriamente diversos apellidos-->
+
+<!DOCTYPE HTML>
+<html lang="es-ES">
+<head>
+	<meta charset="UTF-8">
+	<title>Apellidos Europeos</title>
+</head>
+<body>
+<H3>Generador Aleatorio de Apellidos Europeos</H3>  <H1>Ingrese el numero segun el pais:</H1>
+<form action="Codigo1.php" method="POST">
+	<table>
+	<tr> <td>1: Espana | 2: Italia | 3: Alemania | 4: Francia</td> </tr>
+	<tr> <td> <input type="text" name="num1"> </td> </tr>
+    <tr> <td> <input type="submit" value="Generar"> </td> </tr>
+</table>
+</form>
+</body>
+</html>
