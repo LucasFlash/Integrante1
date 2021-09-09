d8d306c5f14dc6e8a74c9c69fd3e83fe4456b404
Juan <mansillajuanmartin18@gmail.com>
2021-09-09 00:21:20 -0300

    Octavo commit: Se agrega arrays Nombre en PHP

--- Codigo1.php
+++ Codigo1.php
@@ -27,11 +27,16 @@ obtener aleatoriamente diversos apellidos-->
         {	
         $Apellido=array("Garcia", "Rodriguez", "Fernandez", "Lopez", "Sanchez", "Gomez", "Perez", "Gonzales", "Hernandez", "Martinez", "Rossi", "Mancini", "Ferrari","Esposito", "Bianchi", "Romano", "Colombo", "Ricci", "Marino", "Greco", "Muller", "Schmidt", "Schneider", "Fischer", "Weber", "Meyer", "Wagner", "Becker", "Schulz", "Hoffmann",
              "Petit", "Laurent", "Moreau", "Flambeu", "Rousseau", "Blanc", "Matheiu", "Clement", "Monet", "Francois");
+        $Nombres=array("Juan", "Pedro", "Miguel", "Carlos", "Rodrigo", "Fernando", "Gonzalo", "Santiago", "Julian", "Pablo",
+             "Carlo", "Luca", "Giorgio", "Giovagni", "Giuliano", "Andrea", "Benito", "Franco", "Guido", "Luigi",
+             "Otto", "Jurgen", "Franz", "Johann", "Bastian", "oliver", "Miroslav", "Lothar", "Magnus", "Ullrich",
+             "Philip", "Jean Baptiste", "Pierre", "Jean Paul", "Thierry", "Jules", "Dominique", "Fabien", "Lucien", "Antoine");
          $num = $_POST['num1'];
          if ($num < 1) {echo "Error, vuelva a intentar";}
-            	else { if ($num > 4) {echo "Error, vuelva a intentar";} 
-                   else {$s0 = rand(0,9); $p0 = (10 * ( $num - 1) ) + $s0 ;
-	                       echo  $Apellido[$p0];
-	    
+        	else { if ($num > 4) {echo "Error, vuelva a intentar";} 
+                   else {
+	  	                  $s0 = rand(0,9); $p0 = (10 * ( $num - 1) ) + $s0 ;
+	  	                  $s1 = rand(0,9); $p1 = (10 * ( $num - 1) ) + $s1 ;
+	                       echo  $Nombres[$p1] ." " . $Apellido[$p0];
 	    }}}
 ?>
